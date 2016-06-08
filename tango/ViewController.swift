da7c7e478ae7d7c3f629024dbec3d98798958292
内山香 <cosmoslove1107@gmail.com>
2016-06-08 17:17:41 +0900

    Initial Commit

---
+++ tango/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tango
+//
+//  Created by 内山香 on 2016/06/08.
+//  Copyright © 2016年 内山香. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
